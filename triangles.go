d2317bcf3d1cd92469cfb91273fc5d2d9352de59
Ryan <rmfarrell@gmail.com>
2018-07-04 16:53:42 -0400

    fix

--- triangles.go
+++ triangles.go
@@ -6,18 +6,20 @@ import (
 	"time"
 )
 
+type triangle [3]*point
+
 func (m *Mosaic) Triangles(rnd int) string {
 	src := rand.New(rand.NewSource(time.Now().UnixNano()))
 
 	return m.render(func() string {
 
+		var tri triangle
 		x := m.current.X * shapeSize
 		y := m.current.Y * shapeSize
-		triangle := [3]*point{}
 
 		if m.current.X%2 == 0 {
 			// Draw down-pointing triangle
-			triangle = [3]*point{
+			tri = [3]*point{
 				&point{
 					x - 4,
 					y,
@@ -33,7 +35,7 @@ func (m *Mosaic) Triangles(rnd int) string {
 			}
 		} else {
 			// Draw up-pointing triangle
-			triangle = [3]*point{
+			tri = [3]*point{
 				&point{
 					x - 4,
 					y + 10,
@@ -50,19 +52,19 @@ func (m *Mosaic) Triangles(rnd int) string {
 		}
 
 		if rnd > 0 {
-			for _, pt := range triangle {
+			for _, pt := range tri {
 				pt.randomize(*src, rnd)
 			}
 		}
 
 		return fmt.Sprintf(
 			"<polygon points=\"%d,%d %d,%d %d,%d\" fill=\"%s\"/>",
-			triangle[0].x,
-			triangle[0].y,
-			triangle[1].x,
-			triangle[1].y,
-			triangle[2].x,
-			triangle[2].y,
+			tri[0].x,
+			tri[0].y,
+			tri[1].x,
+			tri[1].y,
+			tri[2].x,
+			tri[2].y,
 			m.colorAtCurrent(),
 		)
 	})
